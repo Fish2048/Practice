e30133823270c567f78fbb13fe0488f49c0243ab
小鱼 <294702483@qq.com>
2025-07-18 14:33:05 +0800

    second
    第二天

--- second.py
+++ second.py
@@ -37,5 +37,88 @@ a=(1,2,3,[1,2,4])
 print(a[3][2])
 a=(1,2,3,[1,2,['a','b','c']])
 print(a[3][2][2])
-#运算符
+#运算符 算数运算符
 print('hello'+' '+'world')
+print(5%2) #求余数
+c=1
+c=c+1
+print(c)
+#赋值运算符
+c+=1
+print(c)
+b=3
+a=2
+b+=a
+print(b)
+b-=a
+print(b)
+b*=a
+print(b)
+#比较（关系）运算符 布尔类型
+result=1==1
+print(result)
+a=1
+b=2
+print(a!=b)
+print(1>1)
+b=1
+b+=b>=1
+print(b) #int（True）=1
+c='a'>'b'
+print(c)
+print(ord('a'))
+d='abz'>'adc'  #一个一个字母比较
+print(d)
+#逻辑运算
+print(True and True)
+print('a' and 'b')
+print([] or [1])
+print(True or False)
+print(True or True)
+#or返回第一个
+print ([1] or [2])
+print([2] or [1])  
+#and返回第二个
+print([2] and [1])  
+print([1] and [2])  
+
+#成员运算符
+res= a in [1,2,3,4,5]
+print(res)
+
+
+b='a'
+print(b in {'c':1})
+b='c' #成员运算符是针对key的
+print(b in {'c':1})
+b=1
+print(b in {'c':1})
+
+#身份运算符，两个取值相等，is返回True
+a=1
+b=1.0
+print(a is b) #False
+print(a==b)  
+#关系运算符==是比较值相等
+#身份运算符is比较内存地址
+
+a=1
+b=1
+print(a is b)
+print(id(a))
+print(id(b))
+
+a=[1,2,3]
+b=[2,1,3]
+print(a==b)
+print(a is b)
+
+c=(1,2,3)
+d=(2,1,3)
+print(c==d)
+print(c is d)
+
+e={1,2,3}
+f={2,1,3}
+print(e==f)
+print(e is f)
